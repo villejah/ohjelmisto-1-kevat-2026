798e63ceba55eca45a51b07bf0ee98ac62ae417c
villejah <heikkilaville@icloud.com>
2026-01-21 22:50:19 +0200

    Add files via upload

---
+++ module2/Module2teh3.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+perimeter = (length * 2 + width *2)
+area = (length) * (width)
+print("The perimeter of the rectangle is", (perimeter))
+print("The area of the rectangle is", (area))
